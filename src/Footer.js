e93cafaf7c8e28eb350744f2cb92a5cc9af3b67f
Brunopjunq <bruno_junq@poli.ufrj.br>
2022-07-25 10:59:22 -0300

    Versão Final

--- src/Footer.js
+++ src/Footer.js
@@ -1,13 +1,66 @@
-export default function Footer() {
-    return (
-        <footer>
-            <p>1/4 CONCLUÍDOS</p>
-            <div className="icon-answers">
-            <ion-icon id="zap" name="checkmark-circle"></ion-icon>
-            <ion-icon id="quase-zap" name="help-circle"></ion-icon>
-            <ion-icon id="não-lembrei" name="close-circle"></ion-icon>
-            <ion-icon id="zap" name="checkmark-circle"></ion-icon>
-            </div>
-        </footer>
-    )
+export default function Footer(props) {
+    const {numQts,answers,rightAnswers,IconsAnswer,} = props
+    
+    if(numQts === 0 || answers === 0) {
+        return (
+            <footer>
+                <p>{answers}/{numQts} CONCLUÍDOS</p>
+            </footer>
+        )
+    }
+    else if(answers < numQts) {
+        return (
+            <footer>
+                <p>{answers}/{numQts} CONCLUÍDOS</p>
+                <div className="icon-answers">
+                {IconsAnswer.map(icon => ShowIconsResult(icon))}
+                </div>
+            </footer>
+        )
+    }
+    else {
+        if(rightAnswers < numQts) {
+            return (
+                <footer className="result-msg">
+                    <p>
+                        <img src="assets/img/sad.png" />
+                        <span>Putz...</span>
+                    </p>
+                    <p>Ainda faltam alguns... Mas não desanime!</p>
+                    <p>{rightAnswers}/{numQts} CONCLUÍDOS</p>
+                    <div className="icon-answers">
+                        {IconsAnswer.map(icon => ShowIconsResult(icon))}
+                    </div>
+                </footer>
+            )
+        }
+        else {
+            return (
+                <footer className="result-msg">
+                    <p>
+                        <img src="assets/img/party.png" />
+                        <span>Parabéns!</span>
+                    </p>
+                    <p>Você não esqueceu de nenhum flashcard!</p>
+                    <p>{rightAnswers}/{numQts} CONCLUÍDOS</p>
+                    <div className="icon-answers">
+                        {IconsAnswer.map(icon => ShowIconsResult(icon))}
+                    </div>
+                </footer>
+            )
+        }
+    }
+    
+}
+
+function ShowIconsResult (icon) {
+    if(icon === 'close-circle') {
+        return <ion-icon id="não-lembrei" name="close-circle"></ion-icon>
+    }
+    else if(icon === 'help-circle') {
+        return <ion-icon id="quase-zap" name="help-circle"></ion-icon>
+    } 
+    else if(icon === 'checkmark-circle') {
+        return <ion-icon id="zap" name="checkmark-circle"></ion-icon>
+    }
 }
--- src/TelaRecall.js
+++ src/TelaRecall.js
@@ -13,45 +13,7 @@ export default function TelaRecall() {
         <div>
             <Header />
             <Cards answers={answers} setAnswers={setAnswers} rightAnswers={rightAnswers} setRightAnsewrs={setRightAnsewrs} setNumQts={setNumQts} IconsAnswer={IconsAnswer}/>
-            {/* <div className="flash-cards">
-                <div className="flash-card-question">
-                    <h1>O que é JSX?</h1>
-                    <img src="assets/img/setinha.png" />
-                </div>
-                <div className="flash-card-options">
-                    <h1>JSX é uma sintaxe para escrever HTML dentro do JS</h1>
-                    <div className="button-options">
-                        <button>Não lembrei</button>
-                        <button>Quase não lembrei</button>
-                        <button>Zap!</button>
-                    </div>
-                </div>
-                <div className="flash-card-result zap">
-                    <h1>Pergunta 3</h1>
-                    <ion-icon id="zap" name="checkmark-circle"></ion-icon>
-                </div>
-                <div className="flash-card-result quase-zap">
-                    <h1>Pergunta 4</h1>
-                    <ion-icon id="quase-zap" name="help-circle"></ion-icon>
-                </div>
-                <div className="flash-card-result não-lembrei">
-                    <h1>Pergunta 5</h1>
-                    <ion-icon id="não-lembrei" name="close-circle"></ion-icon>
-                </div>
-                <div className="flash-card">
-                    <h1>Pergunta 6</h1>
-                    <ion-icon name="play-outline"></ion-icon>
-                </div>
-                <div className="flash-card">
-                    <h1>Pergunta 7</h1>
-                    <ion-icon name="play-outline"></ion-icon>
-                </div>
-                <div className="flash-card">
-                    <h1>Pergunta 8</h1>
-                    <ion-icon name="play-outline"></ion-icon>
-                </div>
-            </div> */}
-            <Footer />
+            <Footer numQts = {numQts} IconsAnswer = {IconsAnswer} rightAnswers = {rightAnswers} answers = {answers}/>
         </div>
     )
 }
